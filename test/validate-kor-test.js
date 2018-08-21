24d8236d5b0517d8b351deb702365cf077a8491f
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-08-21 07:15:17 +0200

    Adds test (patch)

---
+++ test/validate-kor-test.js
@@ -0,0 +1,36 @@
+const test = require('ava')
+const validateKor = require('../lib/validate-kor')
+
+test('it returns true for organizations', t => {
+  const data = true
+  const validatedKor = validateKor(data)
+  t.deepEqual(validatedKor, true, 'orgs ok')
+})
+
+test('it returns true for people not reserved', t => {
+  const data = {
+    personer: [
+      {
+        personidentifikator: '26118642424',
+        reservasjon: 'NEI'
+      }
+    ]
+  }
+
+  const validatedKor = validateKor(data)
+  t.deepEqual(validatedKor, true, 'unreserved ok')
+})
+
+test('it returns false for people reserved', t => {
+  const data = {
+    personer: [
+      {
+        personidentifikator: '26118642424',
+        reservasjon: 'JA'
+      }
+    ]
+  }
+
+  const validatedKor = validateKor(data)
+  t.deepEqual(validatedKor, false, 'reserved ok')
+})
